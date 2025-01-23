1d375d8c7668dcc5eed1bd385162df9ff9f03713
puhakkn2 <noora.puhakka@aalto.fi>
2025-01-23 16:10:42 +0200

    add docker

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,9 +5,11 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3003',
+        target: 'http://backend_service:3003',
         changeOrigin: true,
       },
     },
